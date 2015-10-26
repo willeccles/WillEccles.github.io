9e254b1bcb3974273bbb7282945b0204758741d8
Will Eccles <willeccles860+github@gmail.com>
2015-10-26 18:01:18 -0400

    testing loading an entry from a file

--- blog.js
+++ blog.js
@@ -16,7 +16,12 @@ window.onload = function() {
 	var client = new XMLHttpRequest();
 	client.open('GET', dir() + '/entries/ebola.txt');
 	client.onreadystatechange = function() {
-		alert(client.responseText);
+		var entryParts = client.responseText.split("\n");
+		
+		var entryTitle = entryParts[0].replace(new RegExp("TITLE: *"), "");
+		var entryDate = entryParts[1].replace(new RegExp("DATE: *"), "");
+		
+		blogArea.innerHTML += entryHTML(entryTitle, entryDate, entryParts);
 	}
 	client.send();
 };
@@ -31,4 +36,69 @@ function dir() {
 	var dir = loc.substring(0, loc.lastIndexOf("/"));
 	
 	return dir;
+}
+
+// returns the html version of the entry to be placed on the page
+function entryHTML(title, date, contentArray) {
+	var entry = '<div class="entry">';
+
+	var content = "";
+	
+	for (var i = 2; i < contentArray.length; i++) {
+		content += contentArray[i];
+	}
+
+	entry += '\n<div class="entryTitle">' + title + '</div>';
+	entry += '\n<div class="entryDate">' + parseDate(date) + '</div>';
+	entry += '\n<p class="entryPreview">' + content.trim() + '</div>';
+}
+
+// parse a date. 10 26 2015 = October 26, 2015
+function parseDate(date) {
+	var dateParts = date.split(" ");
+	
+	var month;
+	var day = dateParts[1];
+	var year = dateParts[2];
+	
+	switch(dateParts[0]) {
+		case "1":
+			month = "January";
+			break;
+		case "2":
+			month = "February";
+			break;
+		case "3":
+			month = "March";
+			break;
+		case "4":
+			month = "April";
+			break;
+		case "5":
+			month = "May";
+			break;
+		case "6":
+			month = "June";
+			break;
+		case "7":
+			month = "July";
+			break;
+		case "8":
+			month = "August";
+			break;
+		case "9":
+			month = "September";
+			break;
+		case "10":
+			month = "October";
+			break;
+		case "11":
+			month = "November";
+			break;
+		case "12":
+			month = "December";
+			break;
+	}
+	
+	return month + " " + day + ", " + year;
 }
