5ceff2fac549ac34be33afc53c33c1a71e5a9397
Will Eccles <willeccles860+github@gmail.com>
2016-03-31 16:27:42 -0400

    Began working on script.

--- stories/script.js
+++ stories/script.js
@@ -4,3 +4,11 @@ var date = document.getElementById("storyDate").innerHTML;
 window.onload = function() {
 		
 };
+
+// this function should return the string to SHOW to the user
+// a different method should find out which date to show, based on the file
+function dateString() {
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var days = ["Sunday", "Monday", "Tuesday", ]
+
+}
