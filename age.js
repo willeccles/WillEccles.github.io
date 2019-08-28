6fabe65cc407a9a960948ef2b0de835ac24cfd88
Will Eccles <ghostyx123@gmail.com>
2019-08-28 19:32:58 -0400

    redesigned a lot

--- age.js
+++ age.js
@@ -6,9 +6,13 @@ var dateConversion = 1000 * 60 * 60 * 24 * 365;
 // my age in years
 var ageYears = Math.floor(ageMS / dateConversion);
 
+function age() {
+    return ageYears;
+}
+
 function ageString() {
 	var a = "a " + ageYears + "-year-old";
 	var an = "an " + ageYears + "-year-old";
 
-	return (new RegExp("(18|8[0-9])").test(ageYears.toString())) ? an : a;
+	return (/(18|8[0-9])/.test(ageYears.toString())) ? an : a;
 }
